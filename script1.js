c7cd02e21dd2d8ad223fa242aaa0a1ce32d7e203
Witni <witni.whittle@icloud.com>
2020-07-19 11:50:18 -0500

    add js script

--- script1.js
+++ script1.js
@@ -2,11 +2,39 @@ function addNew(){
     var li= document.createElement("li");
     var inputValue = document.getElementById("task-name").value;
     var list = document.querySelector('ul');
-    li.append(inputValue);
+  
+    var task = `<span class="delete">&times;</span><input type="checkbox" class="done"> <label>${inputValue}</label>`;
+    li.innerHTML = task;
     console.log(inputValue);
-
     document.getElementById("task-list").append(li);
-   
+}
+
+
+var list = document.querySelector('ul');
+list.addEventListener('click',function(ev) {
+    console.log (ev);
+    if (ev.target.className === 'done') {
+        ev.target.nextSibling.nextSibling.classList.toggle('checked');
+    
+    }
+    if (ev.target.className == 'delete') {
+        console.log("delete this task");
+        let task = ev.target.parentNode;
+		let list = task.parentNode;
+		list.removeChild(task);
+    }
+    
+}, false);
+
+
+function clearAll(){
+    console.log("make clear all work");
+    var list = document.getElementById("task-list");
+    list.innerHTML = null;
 
 }
+
+
+
+
 
